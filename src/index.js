0c3a13ff53c031f16d245f92861cb7a576b32457
Lucas Bandeira <lucasbandeirai40@gmail.com>
2023-07-22 14:49:30 -0300

    handlebars config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import Express from "express";
-import post from  './modules/Post.js';
-
+import post from "./modules/Post.js";
+import { engine } from "express-handlebars";
 const app = Express();
+
+//config handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.send("ola mundo");
 });
+
+app.get("/cad", (req, res) => {
+  res.render("formulario");
+});
+
+app.post('/add',(req,res)=>{
+  res.send('form recebido')
+})
 
 export default app;
